e6a28a6e4c5e4f5dac2804aad2541b1a480451ef
Binh Nguyen <posseidon@gmail.com>
2019-01-03 23:58:32 +0100

    Lombok on MQProperties

--- src/main/java/io/github/posseidon/mq/MQProperties.java
+++ src/main/java/io/github/posseidon/mq/MQProperties.java
@@ -1,33 +1,11 @@
 package io.github.posseidon.mq;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class MQProperties {
     String amqp, exchange;
     QDef listenOn;
-
-    public String getAmqp() {
-        return amqp;
-    }
-
-    public void setAmqp(String amqp) {
-        this.amqp = amqp;
-    }
-
-    public String getExchange() {
-        return exchange;
-    }
-
-    public void setExchange(String exchange) {
-        this.exchange = exchange;
-    }
-
-    public QDef getListenOn() {
-        return listenOn;
-    }
-
-    public void setListenOn(QDef listenOn) {
-        this.listenOn = listenOn;
-    }
 }
